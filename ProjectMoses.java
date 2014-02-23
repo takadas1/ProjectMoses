296a5f8cd6ee71fb97252e1c2eaebdee45d98d0f
huawillian <huawillian@gmail.com>
2014-02-23 04:24:35 -0500

    Update ProjectMoses.java

--- ProjectMoses.java
+++ ProjectMoses.java
@@ -6,31 +6,21 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
-
+import java.util.Stack;
 import javax.imageio.ImageIO;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-
 import org.opencv.core.Mat; 
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Rect;
 import org.opencv.highgui.VideoCapture;
 import org.opencv.objdetect.CascadeClassifier;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
-import javax.swing.JFrame;
 import javax.swing.JTextArea;
-import javax.swing.JScrollPane;
-
+import twitter4j.*;
+import twitter4j.auth.AccessToken;
 
 public class ProjectMoses extends Frame implements Runnable
 {
@@ -45,14 +35,21 @@ public class ProjectMoses extends Frame implements Runnable
 	
 	// Game Variables
 	static BufferedImage playerImage;	// Player Image
+	static BufferedImage bossImage;	//Boss Image
+	static BufferedImage explodeImage;	//Explode Image
 	static int playerSize = 100;
 	static int playerYcoor = 850;
-	static String playerImageFile = "./src/SpaceShip.jpg";
+	static String playerImageFile = "./src/spaceship2.gif";
 	static Enemy enemy = new Enemy("Shit", 500, 500);
 	static LinkedList <Enemy> enemies = new LinkedList<Enemy>();
 	static Player player;
 	static boolean game = true;
 	static int score = 0;
+	static boolean boss = false, win = false, lose = false;
+	static String bossImageFile = "./src/boss.jpg";
+	static int bossY=-1000;
+	static String expImageFile = "./src/explode.jpg";
+	static Stack<Enemy> death = new Stack<Enemy>();
 	
 	// Timer for the Game Variables
 	static long start_time;
@@ -92,6 +89,8 @@ public class ProjectMoses extends Frame implements Runnable
 	  playerImage = ImageIO.read(new File(playerImageFile)); // Create Player Image
 	  backgroundImage1 = ImageIO.read(new File(backgroundImageFile)); // Create Background Image
 	  backgroundImage2 = ImageIO.read(new File(backgroundImageFile)); // Create Background Image
+	  bossImage = ImageIO.read(new File(bossImageFile)); // Create Boss Image
+	  explodeImage = ImageIO.read(new File(expImageFile)); // Create Explode Image
 
 	}
 	
@@ -128,10 +127,20 @@ public class ProjectMoses extends Frame implements Runnable
 			back2 = -3000;
 			
 			// Play background music
-			playWav("./src/Party Rock Anthem.wav");
+			Clip clip = null;
+		      try
+		      {
+		          clip= AudioSystem.getClip();
+		          clip.open(AudioSystem.getAudioInputStream(new File("./src/Party Rock Anthem.wav")));
+		          clip.start();
+		      }
+		      catch (Exception exc)
+		      {
+		          exc.printStackTrace(System.out);
+		      }		
 			
-			
-			
+			Clip clip1 = null;
+
 			
 			
 			// Game loop here
@@ -147,6 +156,8 @@ public class ProjectMoses extends Frame implements Runnable
 						checkForCollision(enemy);	// Check for collision with player
 						if(!enemy.alive)
 						{
+							death.add(enemy);
+							
 							enemies.remove(enemy); // Check to see if they are alive
 							int rand = (int)((Math.random()*3)+1);
 							
@@ -168,53 +179,107 @@ public class ProjectMoses extends Frame implements Runnable
 				}catch (Exception ConcurrentModificationException){}	// Continue if this exception occurs
 				
 				// Create new enemy every 2 seconds
-				if(System.currentTimeMillis()-timer>1000)
+				if(System.currentTimeMillis()-timer>1000 && !boss)
 				{
-					if(!list.isEmpty())
+					if(!list.isEmpty() && score < 200) // <-- Change Score Limiter
 					{
 						createEnemy(list.getFirst());
 						list.remove(0);		
 					}
+					else
+						{
+							boss = true;
+							try {
+								clip.stop();
+								
+							      try
+							      {
+							          clip1= AudioSystem.getClip();
+							          clip1.open(AudioSystem.getAudioInputStream(new File("./src/Boss.wav")));
+							          clip1.start();
+							      }
+							      catch (Exception exc)
+							      {
+							          exc.printStackTrace(System.out);
+							      }	
+								bossFunction();
+							} catch (Exception e) {
+							}
+						}
 					timer = System.currentTimeMillis();
+				}
 
+				if(boss)
+				{
+					if(bossY < 0) bossY += 20;
+					
+					// Create new enemy after boss reaches point
+					if(System.currentTimeMillis()-timer>500 && bossY >-100)
+					{
+						if(!list.isEmpty())
+						{
+							createEnemy(list.getFirst());
+							list.remove(0);		
+						}
+						timer = System.currentTimeMillis();
+					}				
 				}
 				
+				if(boss && list.isEmpty()) win = true;
+				
+				if(boss && win)
+				{
+
+						winFunction();
+						game = false;
+						clip1.stop();
+				}
+
+			
+
+								
 				checkForInput();
-				
-		
-				
-				
-				
-				
-				
-				
 				
 				updateBackground();	// Update Background
 				repaint();	// Call Paint after Values have been set
 				
-				if(!player.alive) game = false;	// Break out of loop
-
-				// DO STUFF HERE AFTER PLAYER LOSES
-				
-				
+				if(!player.alive)
+				{
+					game = false;	// Break out of loop
+					lose = true;
+				}
+			}		
+			repaint();	
+			
+			try {
+				postTwitter();
+			} catch (TwitterException e) {
 			}
+
 		} 
 	}
 	
-	  public void paint(Graphics g)
+		
+
+	public void paint(Graphics g)
 	  { 
-		  
 		  if(game)
 		  {
+			  // Background Images
 			  g.drawImage(backgroundImage1, 0, back1, 1000, 2100, this);
 			  g.drawImage(backgroundImage2, 0, back2, 1000, 2100, this);
 			  
+			  // Boss Draws
+			  if(boss) g.drawImage(bossImage, 0, bossY, 1000, 500, this);
+			  
+			  // Data for Lives and Scores
 			  g.setFont(new Font("TimesRoman", Font.PLAIN, 50)); 
 			  g.setColor(Color.white);
 			  g.drawString("LIVES: ", 20, 100);
 			  g.setFont(new Font("TimesRoman", Font.PLAIN, 25)); 
 			  g.drawString("SCORE: " + Integer.toString(score), 750, 100);
 			  
+			  // Player draw functions
 			  if(player!=null)
 			  {
 				  for(int i=0; i<player.lives; i++)
@@ -223,25 +288,46 @@ public class ProjectMoses extends Frame implements Runnable
 				  }
 			  }
 			  
-			  g.drawImage(playerImage, (int) xCoor, playerYcoor, playerSize, playerSize, this);
-			  g.drawRect((int)xCoor, playerYcoor, playerSize, playerSize);
-			  
-			  
+			  g.drawImage(playerImage, (int) xCoor, playerYcoor, playerSize, playerSize, this);			  
+
+			  // Draw Enemy
 			  for(Enemy enemy: enemies)
 			  {
 				  g.setFont(new Font("TimesRoman", Font.PLAIN, 50)); 
-				  g.setColor(Color.gray);
+				  g.setColor(enemy.color);
 				  g.fillRect(enemy.xBlock, enemy.yBlock, enemy.bWidth, enemy.bHeight);
 				  g.setColor(Color.black);
 				  g.drawString(enemy.string, enemy.xCoor, enemy.yCoor);
 			  }
+			  
+			  //Draw Death
+			  while(!death.empty())
+			  {
+				  Enemy en = death.pop();
+				  g.drawImage(explodeImage, en.xBlock, en.yBlock, en.bWidth, en.bHeight, this);	
+			  }
+			  
 		  }
-		  else {
+		  else if(lose){
 			  g.drawImage(backgroundImage1, 0, 0, 1000, 2100, this);
 			  g.setFont(new Font("TimesRoman", Font.PLAIN, 100)); 
 			  g.setColor(Color.white);
-			  g.drawString("GAME OVER", 100, 500);
-			  g.drawString("SCORE: " + Integer.toString(score), 100, 700);
+			  g.drawString("GAME OVER", 100, 400);
+			  g.drawString("SCORE: " + Integer.toString(score), 100, 600);
+			  g.setFont(new Font("TimesRoman", Font.PLAIN, 50)); 
+			  g.drawString("See Twitter for daily scores.", 100, 800);
+
+		  }
+		  else if(win)
+		  {
+			  g.drawImage(backgroundImage1, 0, 0, 1000, 2100, this);
+			  g.setFont(new Font("TimesRoman", Font.PLAIN, 100)); 
+			  g.setColor(Color.white);
+			  g.drawString("YOU WIN!", 100, 400);
+			  g.drawString("SCORE: " + Integer.toString(score), 100, 600);
+			  g.setFont(new Font("TimesRoman", Font.PLAIN, 50)); 
+			  g.drawString("See Twitter for daily scores.", 100, 800);
+			  g.drawImage(playerImage, 400, 850, playerSize, playerSize, this);			  
 		  }
 
 
@@ -350,4 +436,60 @@ public class ProjectMoses extends Frame implements Runnable
 
 	}
 
+	// Reading in data from String
+	public static void readString(String s) throws Exception
+	{
+		Scanner scan = new Scanner(s);
+		list= new LinkedList<String>();
+		while(scan.next() != null)
+		{
+			list.add(scan.next().toLowerCase());
+		}
+	}
+	
+	// Boss Function Fight
+	public static void bossFunction() throws Exception
+	{
+		String str = "Hey, didn't think you'd win that easy did ya? Come at me brahhhhh. looooooong string. Let's see how fast you can type. lolol boom boooom Ahhhhhhhhh 1+1=window ????? Hey, didn't think you'd win that easy did ya? Come at me brahhhhh. looooooong string. Let's see how fast you can type. lolol boom boooom Ahhhhhhhhh 1+1=window ????? Hey, didn't think you'd win that easy did ya? Come at me brahhhhh. looooooong string.";
+		readString(str); // <-- Change Boss STring?
+	}
+
+	// Things to do when you win
+	public static void winFunction()
+	{
+		boss = false;
+		playWav("./src/Victory.wav");		
+	}
+	
+	public static void postTwitter() throws TwitterException
+	{
+	      //Your Twitter App's Consumer Key
+        String consumerKey = "FNHYyyHAOIorxwGNWWX9A";
+ 
+        //Your Twitter App's Consumer Secret
+        String consumerSecret = "eB3GylcC54kCKwwdtRdUYb3HVJZLVQNGyNFZ01aKTyo";
+ 
+        //Your Twitter Access Token
+        String accessToken = "2357658374-U2q4lUn2ty2Q0zpIcXm7Wzve7rOTtUSm04FPlfH";
+ 
+        //Your Twitter Access Token Secret
+        String accessTokenSecret = "Tpz9heRnOq3a6L82FFoPoPgcZHLTWj6EyDOG27d0EAstw";
+ 
+        //Instantiate a re-usable and thread-safe factory
+        TwitterFactory twitterFactory = new TwitterFactory();
+ 
+        //Instantiate a new Twitter instance
+        Twitter twitter = twitterFactory.getInstance();
+ 
+        //setup OAuth Consumer Credentials
+        twitter.setOAuthConsumer(consumerKey, consumerSecret);
+ 
+        //setup OAuth Access Token
+        twitter.setOAuthAccessToken(new AccessToken(accessToken, accessTokenSecret));
+ 
+        //Instantiate and initialize a new twitter status update
+        StatusUpdate statusUpdate = new StatusUpdate("NEW USER HAS REACHED SCORE: " + score + " ON PROJECT MOSES!");
+       
+        Status status = twitter.updateStatus(statusUpdate);
+	}
 }
