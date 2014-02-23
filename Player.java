48a002efba780a8dd59969db8f98c946a36659b0
huawillian <huawillian@gmail.com>
2014-02-22 19:28:58 -0500

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+// Class for the Player
+
+public class Player
+{
+	int lives=3, xCoor=0, yCoor=0, width=100, height=100;
+	
+	public Player(int x, int y, int wid, int hei)
+	{
+		lives = 3;
+		xCoor = x;
+		yCoor = y;
+		width = wid;
+		height = hei;
+	}
+	
+	
+	
+	
+}
